c65e471d5daab41589d9d9d880292bc0a48ebf02
rajiul93 <developer.rajiul@gmail.com>
2025-04-22 13:47:16 +0600

    positiveNegativeCheck solved

---
+++ positiveNegativeCheck.js
@@ -0,0 +1,9 @@
+const positiveNegativeCheck = (number) => {
+    if (number > 0) {
+        return "Positive"
+    } else {
+        return "Negative"
+    }
+}
+
+console.log(positiveNegativeCheck(-20))
