0a9cefa7194ce65511e4a5059dc9f16396e4e251
rajiul93 <developer.rajiul@gmail.com>
2025-04-27 13:03:09 +0600

    removeDuplicate

--- array.js
+++ array.js
@@ -3,7 +3,7 @@ const arrayLength = (array) => {
   return array.length;
 };
 //  console.log(arrayLength(fruits))
-const numbers = [5, 10, 15, 20, 25];
+const numbers = [5, 10,10, 15, 44, 20, 25];
 
 const findMax = (num) => {
   return Math.max(...num);
@@ -19,11 +19,30 @@ const checkNumberExists = (numberArray, value) => {
   return numberArray.includes(value);
 };
 // console.log(checkNumberExists(numbers, 5));
-const sortLargeToSmall =(numberArray)=>{
-return numberArray.sort((a,b)=>b-a)
+const sortLargeToSmall = (numberArray) => {
+  return numberArray.sort((a, b) => b - a);
+};
+// console.log(sortLargeToSmall(numbers))
+
+const sortSmallToLarge = (numberArray) => {
+  return numberArray.sort((a, b) => a - b);
+};
+// console.log(sortSmallToLarge(numbers))
+
+function reverseArray(arr) {
+  return arr.reverse();
 }
-console.log(sortLargeToSmall(numbers))
-const sortSmallToLarge =(numberArray)=>{
-    return numberArray.sort((a,b)=>a-b)
-}
-console.log(sortSmallToLarge(numbers))
+// console.log(reverseArray(numbers));
+
+const removeDuplicate = (number) => {
+  let newArray = [];
+  for (let index = 0; index < number.length; index++) {
+    const element = number[index];
+    if (!newArray.includes(element)) {
+      newArray.push(element);
+    }
+  }
+  return newArray
+};
+
+console.log(removeDuplicate(numbers))
